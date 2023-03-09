591058da154ecc0589e1c6b91aa45a47142dacce
jacobgnewman <jacobgnewman@icloud.com>
2023-03-08 20:49:18 -0800

    test some format changes

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,16 @@
-
 mod document_parsing;
-use document_parsing::{md_to_html};
+use axum::{http::StatusCode, response::Html, routing::get, routing::get_service, Router};
+use document_parsing::md_to_html;
+use log::{error, info};
 use std::fs;
-use log::{debug, error, log_enabled, info};
-use axum::{
-    routing::get,
-    response::Html,
-    http::StatusCode,
-    Router,
-    routing::get_service,
-};
 
-use tower_http::services::ServeFile;
-use tera::{Tera, Context};
 use lazy_static::lazy_static;
 use std::io;
+use tera::{Context, Tera};
+use tower_http::services::ServeFile;
 
 // process tera templates
-lazy_static!{
+lazy_static! {
     pub static ref TEMPLATES: Tera = {
         let mut tera = match Tera::new("templates/**/*.html") {
             Ok(t) => t,
@@ -31,39 +24,39 @@ lazy_static!{
     };
 }
 
-
-#[tokio::main] async fn main() {
+#[tokio::main]
+async fn main() {
     env_logger::init();
 
     let app = Router::new()
         .route("/", get(root))
-        .route("/style.css", 
-            get_service(ServeFile::new("templates/style.css"))
-                .handle_error(|error: io::Error| async move {
+        .route(
+            "/style.css",
+            get_service(ServeFile::new("templates/style.css")).handle_error(
+                |error: io::Error| async move {
                     (
                         StatusCode::INTERNAL_SERVER_ERROR,
                         format!("Interal error: {}", error),
                     )
-                })
+                },
+            ),
         )
         .fallback(fallback);
 
-    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+    axum::Server::bind(&"0.0.0.0:80".parse().unwrap())
         .serve(app.into_make_service())
         .await
         .unwrap();
 }
-async fn root() -> Html<String>{
+async fn root() -> Html<String> {
+    info!("/main request");
     let mut context = Context::new();
     let path = "test.md".to_string();
-    let contents =  fs::read_to_string(path)
-        .unwrap();
-    let html = md_to_html(contents)
-        .expect("index parsing error");
+    let contents = fs::read_to_string(path).unwrap();
+    let html = md_to_html(contents).expect("index parsing error");
     context.insert("content", &html);
-    
-    let main = TEMPLATES.render("main.html", &context)
-        .expect("fail");
+
+    let main = TEMPLATES.render("main.html", &context).expect("fail");
     Html(main)
 }
 
@@ -71,4 +64,3 @@ async fn fallback() -> (StatusCode, &'static str) {
     error!("failed to find thing?");
     (StatusCode::NOT_FOUND, "Not Found")
 }
-
--- test.md
+++ test.md
@@ -5,11 +5,26 @@ url: /12345
 
 # Main header
 
+## h2
+
+## h3
+
+## h4
+
 text
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla id purus velit. Curabitur eu turpis aliquet, molestie mauris vel, finibus justo. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nulla vel maximus sapien, ac mattis odio. Nunc id odio elit. Fusce lectus ligula, posuere ac pretium a, porttitor ut orci. Quisque ultricies maximus cursus. Aenean et ultricies justo. Duis dignissim elit ac molestie porttitor. Etiam elementum risus elit, eu dignissim lectus bibendum in. Donec ut purus id magna lacinia dapibus a sit amet nulla. Curabitur dictum neque at vulputate euismod. Nunc eget tincidunt urna. Nunc egestas tellus ligula, vel mollis libero finibus tempor. Duis blandit at quam sed porta.
+
+Vestibulum volutpat accumsan nulla ut varius. Aenean eu tristique erat. In laoreet eu mauris ut iaculis. In accumsan risus turpis, eu tempor erat posuere sit amet. Proin lacinia nisl erat, et facilisis lectus egestas eget. Vestibulum id quam convallis, commodo magna vitae, auctor est. Nullam viverra posuere ullamcorper. In vitae lectus posuere, faucibus mi sit amet, semper eros. Praesent ultrices arcu vitae viverra dignissim. Aliquam tempor, turpis vitae luctus venenatis, dui magna elementum eros, sit amet eleifend velit arcu ac leo. Interdum et malesuada fames ac ante ipsum primis in faucibus. In consequat dolor quis sodales pretium. Vivamus pharetra lorem felis, id aliquet ligula commodo ac.
+
+
 
 *italics* **Bold** [link](https://google.com)
 
+l
 > blockquote
+
+l
 
 ---
 
